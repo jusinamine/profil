2f6c78cff1486d341db4c5706b2072f413c66340
Amine Benkorreche <amine.benk27@gmail.com>
2018-04-05 13:50:44 +0200

    prooofil

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    
+    $("#profil").slideDown(800);
+    
+});
+
+$('#overlay p').on('click', function() { // div input file -- if u click
+    $('#up-avatar').trigger('click');
+});
+
+
